6a03d2b2e64c1b58eabcdc31812b9af88d8fcc45
Adam Žingor <adazi@seznam.cz>
2019-11-18 16:57:21 +0100

    Added Python script to sync header between files

---
+++ syncTemplate.py
@@ -0,0 +1,29 @@
+import re
+import os
+import yaml
+
+template = yaml.load(open("template.yml"))
+
+targets = ["prihlaska.html", "zadani.html"]
+
+for root, dirs, files in os.walk("purkiada/"):
+    for file in files:
+        targets.append(os.path.join(root, file))
+
+
+for target in targets:
+    print("Target:",target,"... ", end="")
+    f = open(target)
+    content = f.read()
+    f.close()
+
+    find = re.search("\s+<head>\n\s+<meta charset=\"utf-8\">\n\s+<title>(.+)</title>\n((?:.*\n)+)\s+</head>", content);
+    content = content.replace(find[2], template["head"].replace("\\t", "  "))
+
+    find = re.search("\s+<nav>\n((?:.*\n)+)\s+</nav>", content);
+    content = content.replace(find[1], template["nav"].replace("\\t", "  "))
+
+    f = open(target, mode="w")
+    f.write(content)
+    f.close()
+    print("DONE")
